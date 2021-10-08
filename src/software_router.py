b3ae3427fe07e5ed88dd02cc547600845466e818
Martin Norling <norlingjr@gmail.com>
2021-10-08 09:31:17 +0200

    Add rabbitmq connection

--- src/software_router.py
+++ src/software_router.py
@@ -4,10 +4,13 @@ This is a Cytomine software router for use in kubernetes.
 """
 
 import time
+import json
 import logging
+from pika.credentials import PlainCredentials
 
 import yaml
 
+import pika
 import cytomine
 
 from json.decoder import JSONDecodeError
@@ -31,13 +34,40 @@ def connect_to_core(settings):
     """
     logging.info("Connecting to cytomine core")
     try:
-        return cytomine.Cytomine.connect(settings['url'],
+        core = cytomine.Cytomine.connect(settings['url'],
                                          settings['publicKey'],
                                          settings['privateKey'])
+        if core.current_user:
+            return core
     except JSONDecodeError:
         logging.error("Failed to connect to cytomine core")
     except yaml.parser.ParserError:
         logging.error("Failed to read settings yaml file")
+
+def connect_to_rabbitmq(settings):
+    """
+    Connects to rabbitmq using the values in settings. Returns a `connection`.
+    """
+    logging.info("Connecting to rabbitmq")
+    try:
+        connection_params = pika.ConnectionParameters(
+            host=settings['host'],
+            port=settings['port'],
+            credentials=pika.PlainCredentials(settings['username'],
+                                              settings['password'])
+        )
+        return pika.BlockingConnection(connection_params)
+    except pika.exceptions.AMQPConnectionError:
+        logging.error("Couldn't connect to rabbitmq")
+
+def queue_callback(ch, method, properties, body):
+    """
+    Callback function that handles rabbitmq messages for the software router.
+    """
+    msg = json.loads(body)
+    logging.info("Received: %s", msg)
+
+    ch.basic_ack(method.delivery_tag)
 
 if __name__ ==  "__main__":
 
@@ -59,17 +89,40 @@ if __name__ ==  "__main__":
     # initialize variables
     core = None
     settings = None
+    rabbitmq = None
+    channel = None
 
     # Main program loop.
     while True:
+        try:
 
-        if not settings:
-            settings = load_settings_file(args.settings_file)
-            # if the settings have changed we should reconnect to core
-            core = None
+            if not settings:
+                settings = load_settings_file(args.settings_file)
+                # if the settings have changed we should reconnect to core and
+                # rabbitmq
+                core = None
+                rabbitmq = None
 
-        if settings and not core:
-            core = connect_to_core(settings['core'])
+            if settings and not core:
+                core = connect_to_core(settings['core'])
 
+            if settings and core and (not rabbitmq or not rabbitmq.is_open):
+                rabbitmq = connect_to_rabbitmq(settings['rabbitmq'])
+                # connect to the default channel
 
+            if rabbitmq and not channel:
+                channel = rabbitmq.channel()
+                channel.basic_consume(queue=settings['rabbitmq']['queue'],
+                                        on_message_callback=queue_callback)
+                # make sure that the channel exists
+                channel.queue_declare(settings['rabbitmq']['queue'],
+                                      durable=True)
+                channel.start_consuming()
+
+        except pika.exceptions.ChannelClosedByBroker:
+            channel = None
+        # TODO: remove this catch-all exception once the program goes beyond
+        # testing
+        except Exception as e:
+            logging.error("Uncaught exception! %s:%s", type(e), e)
         time.sleep(10)
